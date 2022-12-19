95b9003052b8dceb72d446f3fd8e5915bcd873e8
Zaur Qurbanov <115082219+Horizonttt@users.noreply.github.com>
2022-12-20 01:36:54 +0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,9 @@
+var films= [
+['Zombieland', 'Forrest Gump', 'Inception'],
+['Gladiator', 'Shutter Island','Iron Man'],
+['The Hobbit', 'The Terminal', 'Knives Out'],
+]
+
+
+document.write(films[0][0][0]+films[0][0][7]+films[0][1][9]+films[0][1][2])
+console.log(films[0][0][0]+films[0][0][7]+films[0][1][9]+films[0][1][2]+ " "+films[1][0][0]+films[1][1][2]+films[1][1][6]+films[2][0][6]+films[2][1][10]+films[0][2][8]+films[1][2][2]+films[2][2][3])
